9ca8f9fd6ae41904985db14e34510de18d93882b
qilei123 <qileimail123@gmail.com>
2022-10-14 05:27:04 +0000

    update release

--- test_inference.py
+++ test_inference.py
@@ -1,8 +1,9 @@
 from yolov7gastroscopy.inference import *
 import time
-gastroDiseaseDetector = GastroDiseaseDetect(half =True)
 
-gastroDiseaseDetector.ini_model(model_dir="/data/qilei/DATASETS/WJ_V1/yolov7_single_cls_2/yolov7x-wj_v1/weights/best.pt")
+gastro_disease_detector = GastroDiseaseDetect(half =True)
+
+gastro_disease_detector.ini_model(model_dir="/data/qilei/DATASETS/WJ_V1/yolov7_single_cls_2/yolov7x-wj_v1/weights/best.pt")
 
 image = cv2.imread("/data/qilei/DATASETS/WJ_V1/images/3/IMG_01.00279277.0009.09195700180.jpg")
 
@@ -10,7 +11,7 @@ for i in range(100):
 
     t1 = time.time()
 
-    result = gastroDiseaseDetector.predict(image, formate_result = False)
+    result = gastro_disease_detector.predict(image, formate_result = False)
     # or
     # result = gastroDiseaseDetector(image, formate_result = False)
 
@@ -18,4 +19,4 @@ for i in range(100):
     print(f'({(1E3 * (t2 - t1)):.1f}ms) Inference')
 
 
-gastroDiseaseDetector.show_result_on_image(image,result,'test.jpg')
+gastro_disease_detector.show_result_on_image(image,result,'test.jpg')
