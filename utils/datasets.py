6188baec871e0b6e61e243a127a88490d1ac8e3b
qileimail123 <qileimail123@gmail.com>
2022-12-13 15:13:31 -0500

    train 4-4

--- utils/datasets.py
+++ utils/datasets.py
@@ -1346,7 +1346,7 @@ class LoadCOCO(LoadImagesAndLabels):
                     self.segments.append(segs)
                     self.img_files.append(image_dir)
 
-        with_others = False
+        with_others = True
         if True: #将gastro8-12的5批数据纳入，其中4批用于训练，1批用于测试
             dataset_dirs = ["/data3/qilei_chen/DATA/gastro8-12/协和21-11月~2022-5癌变已标注/协和2021-11月_2022-5癌变_20221121", #该批数据用于测试
                             "/data3/qilei_chen/DATA/gastro8-12/2021-2022年癌变已标注/20221111/2021_2022_癌变_20221111/",
