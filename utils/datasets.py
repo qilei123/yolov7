c7c24cf0ded8b73aeb761ecae4ae68e5475e3bec
qilei123 <qileimail123@gmail.com>
2023-01-03 18:32:55 -0500

    train

--- utils/datasets.py
+++ utils/datasets.py
@@ -1358,7 +1358,7 @@ class LoadCOCO(LoadImagesAndLabels):
                         self.img_files.append(image_dir)
 
         with_others = False
-        if False: #将gastro8-12的5批数据纳入，其中4批用于训练，1批用于测试
+        if True: #将gastro8-12的5批数据纳入，其中4批用于训练，1批用于测试
             dataset_dirs = ["/data3/qilei_chen/DATA/gastro8-12/协和21-11月~2022-5癌变已标注/协和2021-11月_2022-5癌变_20221121", #该批数据用于测试
                             "/data3/qilei_chen/DATA/gastro8-12/2021-2022年癌变已标注/20221111/2021_2022_癌变_20221111/",
                             "/data3/qilei_chen/DATA/gastro8-12/低级别_2021_2022已标注/2021_2022_低级别_20221110/",
@@ -1397,7 +1397,7 @@ class LoadCOCO(LoadImagesAndLabels):
         self.shapes = np.array(self.shapes, dtype=np.float64)
         #self.img_files = list(cache.keys())  # update
         #self.label_files = img2label_paths(cache.keys())  # update
-        n = len(self.shapes)  # number of images
+        n = len(self.img_files)  # number of images
         bi = np.floor(np.arange(n) / batch_size).astype(np.int)  # batch index
         nb = bi[-1] + 1  # number of batches
         self.batch = bi  # batch index of image
