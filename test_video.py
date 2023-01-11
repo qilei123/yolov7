51785b1700492205a58181b3ee1c24186f67e6d3
qilei123 <qileimail123@gmail.com>
2023-01-11 01:08:41 -0500

    3-0-

--- test_video.py
+++ test_video.py
@@ -50,7 +50,7 @@ def process_videos():
 
     #gastro_disease_detector.ini_model(model_dir="single_category.pt")
     
-    model_name ='WJ_V1_with_mfp5-4'
+    model_name ='WJ_V1_with_mfp3-0-1'
     
     model_pt_name = 'best'
     
