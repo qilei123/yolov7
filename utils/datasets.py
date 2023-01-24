5e08aa826956c395397c2b41fb6832cee301532a
qilei123 <qileimail123@gmail.com>
2023-01-23 23:39:00 -0500

    train

--- utils/datasets.py
+++ utils/datasets.py
@@ -1835,7 +1835,7 @@ class LoadCOCOv2(LoadImagesAndLabels):
             #labels, shapes, self.segments = zip(*cache.values())
             #self.labels = list(labels)
             
-        times_tp = 1
+        times_tp = 0
         if True:#协和2015-2021年数据+zz_allothers
             data_list= ['/home/ycao/DEVELOPMENTS/yolov7/data_gc/gastro_cancer_v66/annotations/_data2_zzhang_annotation_erosiveulcer_fine_trainfp0927.json',
                         '/home/ycao/DEVELOPMENTS/yolov7/data_gc/gastro_cancer_v66/annotations/_data2_zzhang_annotation_erosiveulcer_fine_test0928.json']
@@ -1855,7 +1855,7 @@ class LoadCOCOv2(LoadImagesAndLabels):
         
         self.datasets_count.append(len(self.img_files))
                 
-        times_tp = 2
+        times_tp = 1
         if True:#load tp dataset from 2021 and 2022 xiangya videos
             
             append_datas = ["/data2/qilei_chen/DATA/2021_2022gastro_cancers/2021_1",
@@ -2139,7 +2139,7 @@ class LoadCOCOv2(LoadImagesAndLabels):
 
         self.datasets_count.append(len(self.img_files))
 
-        times_tp = 2
+        times_tp = 1
         if True: # add the 协和2015-2021年数据_append data
             append_tp_datas = ["/data2/qilei_chen/DATA/WJ_V1/v1_append_cancer",
                             "/data2/qilei_chen/DATA/WJ_V1/v1_append_high_level"]
@@ -2215,7 +2215,7 @@ class LoadCOCOv2(LoadImagesAndLabels):
         self.datasets_count.append(len(self.img_files))
         
         #xl65versions = ['org','m111','m114','m117','m123']
-        xl65v = 'm114'
+        xl65v = 'm123'
         if True: #將xiaolong挑選的65段奧林巴斯視頻的fp納入到訓練過程中
             append_fp_data_dir = "/data2/qilei_chen/wj_fp_images1"
 
@@ -2227,9 +2227,9 @@ class LoadCOCOv2(LoadImagesAndLabels):
             #    ann_file = 'fp_instances_default_test.json'
             
             if test_mode:
-                ann_files = []
+                ann_files = []#['fp_instances_default_test.json',]
             else:
-                ann_files =['fp_instances_default_train.json','fp_instances_default_test.json']
+                ann_files =['fp_instances_default_train.json',]
             for ann_file in ann_files:
             
                 images_root = append_fp_data_dir
@@ -2307,7 +2307,7 @@ class LoadCOCOv2(LoadImagesAndLabels):
         self.datasets_count.append(len(self.img_files)) #利用这个数据的存储实现每次epoch过程中随机挑选一部分m111的图片数据
 
         with_others = True #训练过程中是否将负样本也纳入进去
-        times_tp = 2
+        times_tp = 1
         only_test = False #只纳入该批数据的测试部分
         if True: #将gastro8-12的5批数据纳入，其中4批用于训练，1批用于测试
             '''
@@ -2343,7 +2343,7 @@ class LoadCOCOv2(LoadImagesAndLabels):
         far_vision = 'fars_118'
                         
         with_others = False
-        times_tp = 2        
+        times_tp = 1        
         if True: #将协和39段视频中挑选的两批远景图片数据集全部纳入训练过程
             if far_vision =='fars_118':
                 dataset_dirs = ['/home/ycao/DATASETS/gastro_cancer/fars_118/xiehe_far_1',
@@ -2367,7 +2367,7 @@ class LoadCOCOv2(LoadImagesAndLabels):
         self.datasets_count.append(len(self.img_files))
 
         with_others = False
-        times_tp = 2        
+        times_tp = 1        
         if True: #将湘雅2021-2022视频中挑选的两批远景图片数据集全部纳入训练过程
             if far_vision == 'fars_118':
                 dataset_dirs = ['/home/ycao/DATASETS/gastro_cancer/fars_118/xiangya_far_2021',
