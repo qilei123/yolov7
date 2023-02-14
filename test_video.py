6bbc3591f105366b3cb6c02180029da9f4d0790d
qilei123 <qileimail123@gmail.com>
2023-02-14 15:45:20 -0500

    test videos

--- test_video.py
+++ test_video.py
@@ -55,16 +55,16 @@ def is_in_periods(frame_id,positive_periods):
 
 def process_videos():
 
-    visualize = False
-    gpu_id = 3
+    visualize = True
+    gpu_id = 2
     conf = 0.3
     
     gastro_disease_detector = GastroDiseaseDetect(half =True,gpu_id=gpu_id,conf = conf)
 
     #gastro_disease_detector.ini_model(model_dir="single_category.pt")
     
-    #model_name ='WJ_V1_with_mfp7-22-2_ppsa'
-    model_name = 'WJ_V1_with_mfp7x-22-2_ppsa'
+    model_name ='WJ_V1_with_mfp7-22-2'
+    #model_name = 'WJ_V1_with_mfp7x-22-2_ppsa_v2'
     print(model_name)
     
     model_pt_name = 'best'
@@ -78,7 +78,7 @@ def process_videos():
     videos_dir = '/home/ycao/DATASETS/gastro_cancer/videos_test/xiehe2111_2205'
 
     #report_images_dir = '/data2/qilei_chen/wj_fp_images1'
-    report_images_dir = videos_dir+'_'+model_name+'_'+model_pt_name+'_roifix'
+    report_images_dir = videos_dir+'_'+model_name+'_'+model_pt_name+'_roifix_'+str(conf)
     if visualize:
         report_images_dir += '_vis'
         
