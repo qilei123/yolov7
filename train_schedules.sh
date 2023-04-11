3d27b997146da810de5722542b99e49fcb8513ee
qilei123 <qileimail123@gmail.com>
2023-04-11 09:38:34 -0400

    train more epoch

--- train_schedules.sh
+++ train_schedules.sh
@@ -1,5 +1,5 @@
 python train_org_dev.py --workers 64 \
-    --device 0 \
+    --device 0,1 \
     --batch-size 64 \
     --data data/wj_v1_with_fp.yaml \
     --cfg cfg/training/yolov7-wj_v1_with_fp.yaml \
@@ -9,7 +9,7 @@ python train_org_dev.py --workers 64 \
     --project 27_yolov7_output/WJ_V1_with_mfp7-22-2-recurrent \
     --exist-ok \
     --c_criteria \
-    --epochs 150
+    --epochs 200
 
 #python train.py --workers 8 --device 1 --batch-size 8 --data data/wj_v1.yaml --img 640 640 --cfg cfg/training/yolov7-wj_v1.yaml --weights 'yolov7.pt' --name yolov7-wj_v1 --hyp data/hyp.scratch.custom.yaml --project /data/qilei/DATASETS/WJ_V1/yolov7_single_cls_2 --exist-ok --single-cls --epochs 110
 
